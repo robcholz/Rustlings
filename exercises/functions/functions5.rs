d2edac8b96755f8b44f5403bf56758c9a66f3bb0
robcholz <84130577+robcholz@users.noreply.github.com>
2025-01-21 18:04:17 -0500

    update

--- exercises/functions/functions5.rs
+++ exercises/functions/functions5.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);

d69a2c532368d78d5918475ff6756a375546fe25
robcholz <84130577+robcholz@users.noreply.github.com>
2025-01-21 16:25:56 -0500

    [feature] completed the test

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(rustbuck: i32) -> i32 {
+    if (rustbuck <= 40) {
+        return 2 * rustbuck;
+    }
+    return rustbuck;
+}
 
 // Don't modify this function!
 #[test]
